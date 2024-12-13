bc07efd73dc4887a9cef9b1137e45dcf50dd60d1
Edmundo Duarte <edmundoduarte1998@gmail.com>
2024-12-13 02:02:29 -0300

    desenvolvimento das páginas

--- frontend/src/pages/cadastro.tsx
+++ frontend/src/pages/cadastro.tsx
@@ -1,42 +1,53 @@
 import {TfiAgenda} from "react-icons/tfi";
 
-export default function cadastro(){ 
+export default function Cadastro(){ 
     return( 
-    <div className="bg-red-950 min-h-screen w-full flex justify-center">
+    <div className="bg-emerald-950 min-h-screen w-full flex justify-center">
     
     <div className="absolute bg-green-950-600 left-5 top-10">
     <TfiAgenda size={100} color="white" />
     </div>
     <h1 className="w-90 text-2xl font-medium text-white absolute top-24 left-32">Organizador de Tarefas</h1>
     
-    <main className="bg-green-950 w-96 h-72 my-48 rounded-2xl border-2 border-white md:min-w-4xl absolute"> 
-    <h1 className="text-3xl font-bold text-white flex justify-center my-3">Login</h1>
+    <main className="bg-green-950 w-80 h-96 my-48 rounded-2xl border-2 border-white md:min-w-4xl absolute"> 
+    <h1 className="text-3xl font-bold text-white flex justify-center my-3">Cadastro</h1>
     
     
-    <form className=" w-full h-full my-12">
+    <form className=" w-full h-full my-4">
     
-    <label className="font-medium text-white text-lg ml-4">Email:</label> 
-    <input type="text" placeholder="Digite o seu email"
+    <label className="font-medium text-white text-lg ml-4 flex flex-col">Informe o seu nome:</label> 
+    <input type="text" placeholder="Digite o seu nome"
     className="h-8 w-72 ml-2.5 rounded-lg absolute justify-center"
     />
     
     
     <div className=" my-10">
-    <label className="font-medium text-white text-lg ml-4">Senha:</label>
-    <input type="text" placeholder="Digite a sua senha"
-    className="h-8 w-72 ml-1.5 rounded-lg absolute justify-center"
+    <label className="font-medium text-white text-lg ml-4 flex flex-col">Informe o seu email:</label>
+    <input type="text" placeholder="Digite o seu email"
+    className="h-8 w-72 ml-2.5 rounded-lg absolute justify-center"
     />
     </div>
     
-    
-    <div className=" w-full justify-center">
+    <div className=" my-11">
+    <label className="font-medium text-white text-lg ml-4 flex flex-col">Defina uma senha:</label>
+    <input type="text" placeholder="Digite sua senha"
+    className="h-8 w-72 ml-2.5 rounded-lg absolute justify-center"
+    />
+    </div> 
+   
+    <div className=" w-full my-16">
     <input 
     type="submit" 
-    value="entrar" 
+    value="cadastrar" 
     className="w-44 bg-white h-10 rounded-xl font-medium cursor-pointer
-     border-black border-2 ml-48"
+     border-black border-2 ml-16"
     ></input>
     </div>
+    
+     <div className=" w-full absolute top-80 my-7 ml-4">
+     <a href="/" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Retornar à página inicial</a>
+     </div>
+
     
     </form>
     
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,1 +1,19 @@
-import cadastro from "../pages/cadastro"; 
+import Cadastro from "../pages/cadastro";
+import Home from "../pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";  
+
+
+export function AppRoutes(){ 
+return( 
+<BrowserRouter>
+      <Routes>
+         <Route path="/" element={<Home/>}></Route>
+         <Route path="/cadastro" element={<Cadastro/>}></Route>
+      </Routes>
+</BrowserRouter>
+
+
+)
+
+}
+
